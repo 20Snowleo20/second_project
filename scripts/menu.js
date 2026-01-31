7114d36f2d6f735dbbbd746b20e65c9cc6d105cd
20Snowleo20 <newaccfortesting25@gmail.com>
2026-02-01 02:15:03 +0300

    Version with full workink mobile and tablet adaptation

--- scripts/menu.js
+++ scripts/menu.js
@@ -42,3 +42,21 @@ if (window.innerWidth > 1024 && mainNav.classList.contains('active')) {
     } 
 }); 
 
+// Кнопка "Наверх"
+const scrollTopButton = document.getElementById('scrollTop'); 
+// Показываем кнопку после скролла
+window.addEventListener('scroll', () => { 
+if (window.scrollY > 300) { 
+        scrollTopButton.classList.add('visible'); 
+    } 
+else { 
+        scrollTopButton.classList.remove('visible'); 
+    } 
+}); 
+// Прокрутка к началу страницы 
+scrollTopButton.addEventListener('click', () => { 
+window.scrollTo({ 
+top: 0, 
+behavior: 'smooth' 
+    }); 
+}); 
